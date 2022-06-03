107b572b86cebf9b28242525bd2087362a6f576d
Evan Murray <evan@auraaudio.io>
2022-06-03 11:57:56 -0500

    Add render resources

--- AUv3/Audio Unit/Plugin/MusicalWeatherPlugin.swift
+++ AUv3/Audio Unit/Plugin/MusicalWeatherPlugin.swift
@@ -4,8 +4,38 @@ import Foundation
 
 public class MusicalWeatherPlugin: AUAudioUnit {
     private let kernelAdapter: DSPKernelAdapter
+    lazy private var inputBusArray: AUAudioUnitBusArray = {
+        AUAudioUnitBusArray(audioUnit: self,
+                            busType: .input,
+                            busses: [kernelAdapter.inputBus])
+    }()
+    lazy private var outputBusArray: AUAudioUnitBusArray = {
+        AUAudioUnitBusArray(audioUnit: self,
+                            busType: .output,
+                            busses: [kernelAdapter.outputBus])
+    }()
+    public override var inputBusses: AUAudioUnitBusArray {
+        return inputBusArray
+    }
+    public override var outputBusses: AUAudioUnitBusArray {
+        return outputBusArray
+    }
     public override init(componentDescription: AudioComponentDescription, options: AudioComponentInstantiationOptions = []) throws {
         kernelAdapter = DSPKernelAdapter()
         try super.init(componentDescription: componentDescription, options: options)
+    }
+    public override func allocateRenderResources() throws {
+        if kernelAdapter.outputBus.format.channelCount != kernelAdapter.inputBus.format.channelCount {
+            throw NSError(domain: NSOSStatusErrorDomain, code: Int(kAudioUnitErr_FailedInitialization), userInfo: nil)
+        }
+        try super.allocateRenderResources()
+        kernelAdapter.allocateRenderResources()
+    }
+    public override func deallocateRenderResources() {
+        super.deallocateRenderResources()
+        kernelAdapter.deallocateRenderResources()
+    }
+    public override var internalRenderBlock: AUInternalRenderBlock {
+        return kernelAdapter.internalRenderBlock()
     }
 }
