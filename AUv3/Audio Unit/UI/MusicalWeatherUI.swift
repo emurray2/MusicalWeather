3572c85232f9b41628dd8ecac2834371aaa5cfec
Evan Murray <evan@auraaudio.io>
2022-06-04 00:44:57 -0500

    Remove storyboard - use `ContentView()` for all UI

--- AUv3/Audio Unit/UI/MusicalWeatherUI.swift
+++ AUv3/Audio Unit/UI/MusicalWeatherUI.swift
@@ -1,24 +1,35 @@
 // Copyright Evan Murray. All Rights Reserved. Revision History at http://github.com/emurray2/MusicalWeather
 
 import CoreAudioKit
+import SwiftUI
 
 public class MusicalWeatherUI: AUViewController, AUAudioUnitFactory {
-    var audioUnit: AUAudioUnit?
-    
+    var audioUnit: MusicalWeatherPlugin? {
+        didSet {
+            // Connect parameters to the view model
+            print("got the audio unit")
+        }
+    }
+
+    let contentView = UIHostingController(rootView: ContentView())
+
     public override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if audioUnit == nil {
-            return
-        }
-        
-        // Get the parameter tree and add observers for any parameters that the UI needs to keep in sync with the AudioUnit
+        addChild(contentView)
+        view.addSubview(contentView.view)
+        setupConstraints()
     }
-    
+
+    fileprivate func setupConstraints() {
+        contentView.view.translatesAutoresizingMaskIntoConstraints = false
+        contentView.view.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        contentView.view.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        contentView.view.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        contentView.view.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+    }
+
     public func createAudioUnit(with componentDescription: AudioComponentDescription) throws -> AUAudioUnit {
         audioUnit = try MusicalWeatherPlugin(componentDescription: componentDescription, options: [])
-        
         return audioUnit!
     }
-    
 }
