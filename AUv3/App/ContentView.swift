b19c4aba4163cd77006e438aa3fde9d7156a9bc0
Evan Murray <evan@auraaudio.io>
2022-06-04 22:24:56 -0500

    Wrap everything in scroll view

--- AUv3/App/ContentView.swift
+++ AUv3/App/ContentView.swift
@@ -4,22 +4,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            LinearGradient(colors: [Color(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347), Color(red: 0.9529411793, green: 0.8685067713, blue: 0.1800223484)], startPoint: .top, endPoint: .bottom)
-            VStack {
-                Text("Woodbury")
-                    .foregroundColor(.white)
-                    .font(.system(size: 40))
-                Text("Sunny")
-                    .foregroundColor(.white)
-                    .font(.system(size: 25)).padding(.bottom, 60)
-                    .frame(maxWidth: .infinity)
-                Image(systemName: "sun.max.fill")
-                    .foregroundColor(.white)
-                    .font(.system(size: 120))
-                Text("72")
-                    .foregroundColor(.white)
-                    .font(.system(size: 90))
+        ScrollView {
+            ZStack {
+                LinearGradient(colors: [Color(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347), Color(red: 0.9529411793, green: 0.8685067713, blue: 0.1800223484)], startPoint: .top, endPoint: .bottom)
+                VStack {
+                    Text("Woodbury")
+                        .foregroundColor(.white)
+                        .font(.system(size: 40))
+                    Text("Sunny")
+                        .foregroundColor(.white)
+                        .font(.system(size: 25)).padding(.bottom, 60)
+                        .frame(maxWidth: .infinity)
+                    Image(systemName: "sun.max.fill")
+                        .foregroundColor(.white)
+                        .font(.system(size: 120))
+                    Text("72")
+                        .foregroundColor(.white)
+                        .font(.system(size: 90))
+                }
             }
         }
         .edgesIgnoringSafeArea(.all)
